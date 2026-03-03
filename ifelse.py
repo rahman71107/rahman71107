9bfa1e6b9216846d1b01f27e0ef33647ce8e748f
rahman71107 <rahman71107_bec28@mepcoeng.ac.in>
2026-03-03 16:04:36 +0530

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,24 @@
+Python - If Else
+Author    : Abdul Hameed
+
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    # Check if n is odd
+    if n % 2 != 0:
+        print("Weird")
+    # If even, check the ranges
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
